e45e9525dfe5d8486af90ba670f6cce8816a724e
LuiCoro <luiacorona029@icloud.com>
2021-08-10 21:22:31 -0700

    'Added' id to team data

--- team-builder/src/Components/Data.js
+++ team-builder/src/Components/Data.js
@@ -1,23 +1,27 @@
 const team = [
 	{
+		id: 1,
 		name: 'Blake Deeer',
 		age: '26',
 		role: 'Duck Hunter',
 		email: 'blakehunt@quack.com',
 	},
 	{
+		id: 2,
 		name: 'Bob Summy',
 		age: '36',
 		role: 'Doctor',
 		email: 'BobMed@med.com',
 	},
 	{
+		id: 3,
 		name: 'Ken Smith',
 		age: '31',
 		role: 'Artist',
 		email: 'kenArt@art.com',
 	},
 	{
+		id: 4,
 		name: 'Sam Lopez',
 		age: '24',
 		role: 'Detective',
