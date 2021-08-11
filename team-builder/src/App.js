e8aa467ea7a5f0c74ffa01cbe5a26a8897fb8135
LuiCoro <luiacorona029@icloud.com>
2021-08-10 21:39:58 -0700

    'Setting Up' Setting Up form & rendered team

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -15,22 +15,40 @@ const defaultValues = [
 
 console.log(defaultValues);
 const App = () => {
+	const [formValues, setFormValues] = useState(defaultValues);
+	const [teamList, setTeamList] = useState(team);
+
+	console.log('Team List', teamList);
+	console.log('Form Values', formValues);
 	return (
 		<div>
 			<form>
 				<label>
-					Name: <input />
+					Name: <input type='text' name='Name' placeholder='Enter Name' />
 				</label>
 				<label>
-					Age: <input />
+					Age: <input type='number' name='Age' placeholder='Enter Age' />
 				</label>
 				<label>
-					Role: <input />
+					Role: <input type='text' name='Role' placeholder='Enter Role' />
 				</label>
 				<label>
-					Email: <input />
+					Email: <input type='email' name='Email' placeholder='Enter Email' />
 				</label>
+
+				<button>Submit</button>
 			</form>
+
+			{teamList.map(user => {
+				return (
+					<div>
+						<h1>{user.name}</h1>
+						<h3>{user.role}</h3>
+						<p>{user.age}</p>
+						<p>{user.email}</p>
+					</div>
+				);
+			})}
 		</div>
 	);
 };
