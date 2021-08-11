b30347484c848301db27258e00430b26b6a18efd
LuiCoro <luiacorona029@icloud.com>
2021-08-10 22:15:20 -0700

    'Setting Up' Members Component so each user render

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,20 +1,32 @@
 import React, { useState } from 'react';
 
 import team from './Components/Data';
+import Members from './Components/Members';
 
 console.log(team);
 
-const defaultValues = 
-	{
-		name: '',
-		age: '',
-		role: '',
-		email: '',
-	},
-;
+const defaultValues = {
+	name: '',
+	age: '',
+	role: '',
+	email: '',
+};
 
 console.log(defaultValues);
+
 const App = () => {
+	const [teamList, setTeamList] = useState([]);
+
+	const [formValues, setFormValues] = useState(defaultValues);
+	return (
+		<div>
+			<h1>Hello worrld</h1>
+
+			{team.map(user => {
+				return <Members />;
+			})}
+		</div>
+	);
 };
 
 export default App;
